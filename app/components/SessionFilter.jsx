e3943f8fb786e7decb390440c9b0db6f3401079e
Lauren Clark <lclark@scottlogic.com>
2016-04-05 09:01:00 -0700

    BRITECHSRM-138 Filter styling

--- app/components/SessionFilter.jsx
+++ app/components/SessionFilter.jsx
@@ -1,49 +1,45 @@
 import React, { Component, PropTypes } from 'react';
 import immutable from 'immutable';
-import Checkbox from 'material-ui/lib/checkbox';
+import SessionFilterCheckbox from './SessionFilterCheckbox.jsx';
 
 class SessionFilter extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    handleCheck(status, checked) {
+        var newFilter = this.props.options.set(status, checked);
+        this.props.onChange(newFilter);
+    }
 
-  handleCheck(status, checked) {
-    var newFilter = this.props.options.set(status, checked);
-    this.props.onChange(newFilter);
-  }
-
-  render() {
-    return (
-      <div>
-        <Checkbox
-            key={1}
-            label='Assigned'
-            checked={this.props.options.get('1') || false}
-            onCheck={(e, c) => this.handleCheck('1', c)} />
-        <Checkbox
-            key={2}
-            label='In Progress'
-            checked={this.props.options.get('2') || false}
-            onCheck={(e, c) => this.handleCheck('2', c)} />
-        <Checkbox
-            key={3}
-            label='Deferred'
-            checked={this.props.options.get('3') || false}
-            onCheck={(e, c) => this.handleCheck('3', c)} />
-        <Checkbox
-            key={4}
-            label='Topic Approved'
-            checked={this.props.options.get('4') || false}
-            onCheck={(e, c) => this.handleCheck('4', c)} />
-        <Checkbox
-            key={5}
-            label='Date Assigned'
-            checked={this.props.options.get('5') || false}
-            onCheck={(e, c) => this.handleCheck('5', c)} />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <SessionFilterCheckbox
+                    isChecked={this.props.options.get('1') || false}
+                    label='Assigned'
+                    status='assigned'
+                    onCheck={c => this.handleCheck('1', c)} />
+                <SessionFilterCheckbox
+                    isChecked={this.props.options.get('2') || false}
+                    label='In progress'
+                    status='in-progress'
+                    onCheck={c => this.handleCheck('2', c)} />
+                <SessionFilterCheckbox
+                    isChecked={this.props.options.get('3') || false}
+                    label='Deferred'
+                    status='deferred'
+                    onCheck={c => this.handleCheck('3', c)} />
+                <SessionFilterCheckbox
+                    isChecked={this.props.options.get('4') || false}
+                    label='Topic approved'
+                    status='topic-approved'
+                    onCheck={c => this.handleCheck('4', c)} />
+                <SessionFilterCheckbox
+                    isChecked={this.props.options.get('5') || false}
+                    label='Date assigned'
+                    status='date-assigned'
+                    onCheck={c => this.handleCheck('5', c)} />
+            </div>
+        )
+    }
 }
 
 export default SessionFilter;
