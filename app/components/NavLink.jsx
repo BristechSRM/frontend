e064dff4e72ee4c598d5d9fd60ddc6d24a95e6ad
Lauren Clark <lclark@scottlogic.com>
2016-04-07 04:36:33 -0700

    Fix NavLink warnings

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component, PropTypes } from 'react';
 import RaisedButton from 'material-ui/lib/raised-button';
 
 class NavLink extends Component {
 
-    handleClick(e) {
+    handleClick() {
     }
 
     render() {
@@ -14,9 +14,14 @@ class NavLink extends Component {
         const styles = Object.assign({}, defaultStyles, this.props.styles);
 
         return (
-          <RaisedButton label={this.props.label} onClick={e => this.handleClick} style={styles}/>
+          <RaisedButton label={this.props.label} onClick={() => this.handleClick} style={styles}/>
         )
     }
 }
+
+NavLink.propTypes = {
+    label: PropTypes.string,
+    styles: PropTypes.object
+};
 
 export default NavLink;
