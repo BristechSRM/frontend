7ca2af14e2911df0b6087a758911ee03edc500a9
Lauren Clark <lclark@scottlogic.com>
2016-04-07 03:17:34 -0700

    BRITECHSRM-142 Session details view

---
+++ app/components/SessionCorrespondence.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import CorrespondenceItem from './CorrespondenceItem.jsx';
+
+import styles from './sessionCorrespondence.scss';
+
+class SessionCorrespondence extends Component {
+
+
+    render() {
+
+        return (
+            <div className={styles.correspondence}>
+                {this.props.correspondence.map(correspondenceItem =>
+                    <div className={styles.correspondenceItem}>
+                        <CorrespondenceItem
+                            message={correspondenceItem.message}
+                            date={correspondenceItem.date} />
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+export default SessionCorrespondence;
