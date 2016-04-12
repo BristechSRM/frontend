dadbb8897be82cecbe6ab0296297e8d09de997bb
Lauren Clark <lclark@scottlogic.com>
2016-04-12 06:50:10 -0700

    Add tbody to tables in session view

--- app/components/SessionSidebar.jsx
+++ app/components/SessionSidebar.jsx
@@ -30,56 +30,60 @@ class SessionSidebar extends Component {
                 <div className={styles.section}>
                     <h1>Status</h1>
                     <table>
-                        <tr>
-                            <td>Assigned Admin</td>
-                            <td>{this.getName(this.props.admin)}</td>
-                        </tr>
-                        <tr>
-                            <td>Credibility</td>
-                            <td>
-                              <StarRating
-                                name="session-rating"
-                                totalStars={5}
-                                rating={this.props.speaker.rating}
-                                disabled
-                                size={16}
-                              />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Signup Method</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Date Added</td>
-                            <td>2 November 2015</td>
-                        </tr>
-                        <tr>
-                            <td>Last Contact</td>
-                            <td>2 January 2016 at 3:24pm</td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>Assigned Admin</td>
+                                <td>{this.getName(this.props.admin)}</td>
+                            </tr>
+                            <tr>
+                                <td>Credibility</td>
+                                <td>
+                                  <StarRating
+                                    name="session-rating"
+                                    totalStars={5}
+                                    rating={this.props.speaker.rating}
+                                    disabled
+                                    size={16}
+                                  />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Signup Method</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Date Added</td>
+                                <td>2 November 2015</td>
+                            </tr>
+                            <tr>
+                                <td>Last Contact</td>
+                                <td>2 January 2016 at 3:24pm</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
 
                 <div className={styles.section}>
                     <h1>Contact Details</h1>
                     <table>
-                        <tr>
-                            <td>Email</td>
-                            <td>{this.getHandle('email')}</td>
-                        </tr>
-                        <tr>
-                            <td>Phone Number</td>
-                            <td>{this.getHandle('phone')}</td>
-                        </tr>
-                        <tr>
-                            <td>Twitter</td>
-                            <td>@{this.getHandle('twitter')}</td>
-                        </tr>
-                        <tr>
-                            <td>Github</td>
-                            <td>{this.getHandle('github')}</td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>Email</td>
+                                <td>{this.getHandle('email')}</td>
+                            </tr>
+                            <tr>
+                                <td>Phone Number</td>
+                                <td>{this.getHandle('phone')}</td>
+                            </tr>
+                            <tr>
+                                <td>Twitter</td>
+                                <td>@{this.getHandle('twitter')}</td>
+                            </tr>
+                            <tr>
+                                <td>Github</td>
+                                <td>{this.getHandle('github')}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
