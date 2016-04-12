e1b9cecdeea6d61fdba7ee7ab107d2ff17c48c04
ilovell <ilovell@scottlogic.co.uk>
2016-04-12 16:35:07 +0100

    Updated session card to show last contact as date

--- app/services/SessionsApi.js
+++ app/services/SessionsApi.js
@@ -6,8 +6,19 @@ const sessionsUri = 'http://api.bris.tech/sessions';
 
 class SessionsApi {
 
+    getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // TODO: /sessions endpoint should return threadId for each sessions
+    // currently assigning a fixed value
     getAllSessions() {
-        return api.get(sessionsUri);
+        return api.get(sessionsUri)
+            .then(sessions => sessions.map(s => {
+                const updated = s;
+                updated.threadId = this.getRandom(1, 2).toString();
+                return s;
+            }));
     }
 
     filterAndSort(sessions, filters, sortProperty, isSortOrderAscending) {
