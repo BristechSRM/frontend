03c93d4272ac3e6b1fc7ae2a1b33547289446f45
Lauren Clark <lclark@scottlogic.com>
2016-04-15 02:24:50 -0700

    Move action types to constants file

--- app/reducers/session.js
+++ app/reducers/session.js
@@ -1,13 +1,6 @@
 import { handleActions } from 'redux-actions';
 import immutable from 'immutable';
-import {
-    GET_SESSION_START,
-    GET_SESSION_COMPLETE,
-    GET_SESSION_ERROR,
-    GET_PROFILES_START,
-    GET_PROFILES_COMPLETE,
-    GET_PROFILES_ERROR,
-} from '../actions';
+import * as actionTypes from '../constants/actionTypes';
 
 const initialState = immutable.Map({
     isFetching: false,
@@ -17,7 +10,7 @@ const initialState = immutable.Map({
 });
 
 const session = handleActions({
-    [GET_SESSION_START]: (state) =>
+    [actionTypes.GET_SESSION_START]: (state) =>
         state.withMutations(map => {
             map.set('isFetching', true)
                 .set('session', {})
@@ -25,31 +18,31 @@ const session = handleActions({
                 .set('admin', {});
         }),
 
-    [GET_SESSION_ERROR]: (state, action) =>
+    [actionTypes.GET_SESSION_ERROR]: (state, action) =>
         state.withMutations(map => {
             map.set('isFetching', false)
                 .set('error', action.payload);
         }),
 
-    [GET_SESSION_COMPLETE]: (state, action) =>
+    [actionTypes.GET_SESSION_COMPLETE]: (state, action) =>
         state.withMutations(map => {
             map.set('isFetching', false)
                 .set('session', action.payload);
         }),
 
-    [GET_PROFILES_START]: (state) =>
+    [actionTypes.GET_PROFILES_START]: (state) =>
         state.withMutations(map => {
             map.set('speaker', {})
                 .set('admin', {});
         }),
 
-    [GET_PROFILES_COMPLETE]: (state, action) =>
+    [actionTypes.GET_PROFILES_COMPLETE]: (state, action) =>
         state.withMutations(map => {
             map.set('speaker', action.payload.speaker)
                 .set('admin', action.payload.admin);
         }),
 
-    [GET_PROFILES_ERROR]: (state) =>
+    [actionTypes.GET_PROFILES_ERROR]: (state) =>
         state.withMutations(map => {
             map.set('speaker', {})
                 .set('admin', {});
