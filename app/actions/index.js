ba1fcce1d2095b820806863e6c0ccdf544dcc251
ilovell <ilovell@scottlogic.co.uk>
2016-04-22 14:39:51 +0100

    Renamed service types

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,8 +1,6 @@
-import {
-    createAction,
-} from 'redux-actions';
-import SessionsApi from '../services/SessionsApi';
-import CommsApi from '../services/CommsApi';
+import { createAction } from 'redux-actions';
+import SessionsService from '../services/SessionsService';
+import CommsService from '../services/CommsService';
 import ProfilesService from '../services/ProfilesService';
 import immutable from 'immutable';
 import * as actionTypes from '../constants/actionTypes';
@@ -10,20 +8,23 @@ import * as actionTypes from '../constants/actionTypes';
 export const getAllSessions = () =>
     (dispatch, getState) => {
         dispatch(createAction(actionTypes.UPDATE_SESSIONS_START)());
-        return SessionsApi.getAllSessions()
+        return SessionsService.getAllSessions()
             .then(sessions => dispatch(createAction(actionTypes.UPDATE_SESSIONS_COMPLETE)(immutable.fromJS(sessions))))
             .then(() => dispatch(createAction(actionTypes.UPDATE_LAST_CONTACT_START)()))
-            .then(CommsApi.getLastContact)
-            .then(lastContact => dispatch(createAction(actionTypes.UPDATE_LAST_CONTACT_COMPLETE)(
-                immutable.fromJS(CommsApi.mergeLastContact(lastContact, getState().get('sessions').get('sessions'))))),
-                error => dispatch(createAction(actionTypes.UPDATE_LAST_CONTACT_ERROR)(error)))
+            .then(CommsService.getLastContact)
+            .then(lastContact => {
+                const existingSessions = getState().get('sessions').get('sessions');
+                const sessionsWithLastContact = CommsService.mergeLastContact(lastContact, existingSessions);
+                const immutableSessions = immutable.fromJS(sessionsWithLastContact);
+                dispatch(createAction(actionTypes.UPDATE_LAST_CONTACT_COMPLETE)(immutableSessions));
+            })
             .catch(error => dispatch(createAction(actionTypes.UPDATE_SESSIONS_ERROR)(error)));
     };
 
 export const getSession = (sessionId) =>
     (dispatch) => {
         dispatch(createAction(actionTypes.GET_SESSION_START)());
-        return SessionsApi.getSession(sessionId)
+        return SessionsService.getSession(sessionId)
             .then(session => dispatch(createAction(actionTypes.GET_SESSION_COMPLETE)(session)))
             .catch(error => dispatch(createAction(actionTypes.GET_SESSION_ERROR)(error)));
     };
@@ -49,7 +50,7 @@ export const getAdmin = (profileId) =>
 export const getCorrespondence = (threadId) =>
     (dispatch) => {
         dispatch(createAction(actionTypes.GET_CORRESPONDENCE_START)());
-        return CommsApi.getThread(threadId)
+        return CommsService.getThread(threadId)
             .then(thread => dispatch(createAction(actionTypes.GET_CORRESPONDENCE_COMPLETE)(thread.items)))
             .catch(error => dispatch(createAction(actionTypes.GET_CORRESPONDENCE_ERROR)(error)));
     };
