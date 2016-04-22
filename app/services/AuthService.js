d317d4eecb5b4475eeb2854b47f52136e3bf47e6
ilovell <ilovell@scottlogic.co.uk>
2016-04-22 14:07:13 +0100

    Updated authority port to correct address

--- app/services/AuthService.js
+++ app/services/AuthService.js
@@ -3,7 +3,7 @@
 class AuthService {
     constructor() {
         const settings = {
-            authority: 'http://api.bris.tech:9003',
+            authority: 'http://api.bris.tech:8083',
             client_id: 'bristechsrm',
             redirect_uri: 'http://srm.bris.tech/signed-in',
             post_logout_redirect_uri: 'http://srm.bris.tech',
