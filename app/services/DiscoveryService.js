42fa80d965bac2332d5b4de7fecf39075ae7a93b
ilovell <ilovell@scottlogic.co.uk>
2016-05-09 09:37:57 +0100

    Moved services out

--- app/services/DiscoveryService.js
+++ app/services/DiscoveryService.js
@@ -1,23 +1,23 @@
 import urlJoin from 'url-join';
 
+const services = {
+    ['bristech-srm']: {
+        development: 'http://localhost:8080',
+        production: 'http://srm.bris.tech',
+    },
+    ['api-gateway']: {
+        development: 'http://apigateway:8080',
+        production: 'http://api.bris.tech:8080',
+    },
+    ['auth']: {
+        development: 'http://auth:8080',
+        production: 'http://auth.bris.tech',
+    },
+};
+
 class DiscoveryService {
 
     registerServices(environment) {
-        const services = {
-            ['bristech-srm']: {
-                development: 'http://localhost:8080',
-                production: 'http://srm.bris.tech',
-            },
-            ['api-gateway']: {
-                development: 'http://localhost:9000',
-                production: 'http://api.bris.tech:8080',
-            },
-            ['auth']: {
-                development: 'http://localhost:9001',
-                production: 'http://auth.bris.tech',
-            },
-        };
-
         for (const service in services) {
             if (services.hasOwnProperty(service)) {
                 const serviceDetails = services[service];
